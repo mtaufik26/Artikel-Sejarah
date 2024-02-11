8ee2cfec05933ee4cd903d714a465af38e0caa7e
mtaufik26 <m.taufikediansyah@smkwikrama.sch.id>
2024-02-11 20:32:31 +0700

    Upload

---
+++ main.js
@@ -0,0 +1,40 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var contentElement = document.getElementById('content');
+
+//     var articleElement = document.createElement('article');
+
+//     var titleElement = document.createElement('h2');
+//     titleElement.textContent = 'Pergerakan Nasional Indonesia';
+
+//     var contentText = document.createElement('p');
+//     contentText.textContent = 'Pergerakan Nasional Indonesia merujuk pada serangkaian peristiwa dan aktivitas yang dilakukan oleh masyarakat Indonesia untuk mencapai kemerdekaan dari penjajahan Belanda dan menjunjung tinggi nilai-nilai nasionalisme.';
+
+//     articleElement.appendChild(titleElement);
+//     articleElement.appendChild(contentText);
+
+//     contentElement.appendChild(articleElement);
+// });
+
+
+// Skrip untuk membuat efek smooth scroll
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//     anchor.addEventListener('click', function (e) {
+//         e.preventDefault();
+
+//         document.querySelector(this.getAttribute('href')).scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
+
+// // Skrip untuk membuat galeri gambar
+// const images = document.querySelectorAll('img');
+
+// images.forEach(image => {
+//     image.addEventListener('click', function () {
+//         // Tampilkan gambar dalam modul lightbox
+//         // Anda bisa menggunakan library pihak ketiga seperti Lightbox atau mengimplementasikannya sendiri
+//         // Contoh penggunaan Lightbox: https://lokeshdhakar.com/projects/lightbox2/
+//         alert('Tampilkan gambar dalam modul lightbox atau galeri lainnya');
+//     });
+// });
